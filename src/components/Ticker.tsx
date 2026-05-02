112551c4fa9b5a3bb1826b40d85d08ddc850a2c4
talhaarain461-cell <talhaarain461@mail.com>
2026-05-02 15:52:03 +0500

    update

--- src/components/Ticker.tsx
+++ src/components/Ticker.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Ticker = React.memo(function Ticker() {
-  const tickerText = "WE ARE LIVE NOW • ORDER YOUR PREMIUM MANGOES • ";
+   const tickerText = "PREMIUM MANGOES COMING SOON • STAY TUNED • ";
   const repeatedText = tickerText.repeat(8);
   
   return (
